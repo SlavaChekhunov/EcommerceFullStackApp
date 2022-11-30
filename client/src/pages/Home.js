379c846b0f32856470d785c502e262e100b57770
Slava Chekhunov <blckct097@gmail.com>
2022-11-29 22:54:50 -0500

    finished the single product page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,15 +3,16 @@ import HomeHeader from "../components/HomeHeader";
 import HomeMain from "../components/HomeMain";
 import Footer from "../components/Footer";
 import ProductList from "./ProductList";
+import Product from "./Product";
 
 const Home = () => {
   const homeID = "homeMain"
   return (
     <>
-      <HomeHeader mainID={homeID} />
-      <HomeMain />
-      <Footer />
-      <ProductList />
+      {/* <HomeHeader mainID={homeID} /> */}
+      {/* <HomeMain /> */}
+      {/* <Footer /> */}
+      <Product />
     </>
   )
 }; 
