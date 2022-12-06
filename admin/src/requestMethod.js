125e59ea7a330221745911fa20f3be76f435b68a
Slava Chekhunov <blckct097@gmail.com>
2022-12-06 00:48:17 -0500

    added some redux

---
+++ admin/src/requestMethod.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/";
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNzFjY2VhYzgwM2ExM2IxNmE2MzY5MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTkzNjYyMCwiZXhwIjoxNjcwMTk1ODIwfQ._Zp0kmDb7PNVF5vK2X3VnxNDQEhKEyXamHCPNj2wsOc";
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  header: { token: `Bearer ${TOKEN}` },
+});
